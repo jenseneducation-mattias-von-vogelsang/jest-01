142527d356e4b1fce38183172c0dfee6334fd8a4
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-08-17 16:59:23 +0200

    added assignments & answers for testing with jest

--- js/excersises.js
+++ js/excersises.js
@@ -1,14 +1,75 @@
-// let number;
+//*** Excersise 1 & 2, store & retrieve ***
+let number;
 
-// function store(input) {
-//   number = input;
-// }
+function store(input) {
+  number = input;
+}
 
-// function retrieve() {
-//   return number;
-// }
+function retrieve() {
+  return number;
+}
 
-// export default {
-//   store,
-//   retrieve
-// };
+//*** Excersise 3, capitalize first letter in string ***
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.substring(1);
+}
+
+//*** Excersise 4 ***/
+function multiply(x, y) {
+  return x * y;
+}
+
+//*** Excersise 5 ***/
+function isWaterBoiling(degreesCelsius) {
+  let answer;
+  if (degreesCelsius === "") throw new Error('Input is empty');
+  if (degreesCelsius < -237) throw new Error('Input is too cold');
+  if (isNaN(degreesCelsius)) throw new Error('Input is not a number');
+  if (degreesCelsius >= 100) {
+    answer = true;
+  } else if (degreesCelsius < 100) {
+    answer = false;
+  }
+  return answer;
+}
+
+//*** Excersise ***
+function intToRoman(num) {
+  var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNumeral = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+
+  let romanized = "";
+
+  for (var index = 0; index < decimalValue.length; index++) {
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
+    }
+  }
+
+  return romanized;
+}
+
+//*** EXPORTS ***
+module.exports = {
+  store,
+  retrieve,
+  capitalize,
+  multiply,
+  isWaterBoiling,
+  intToRoman
+};
--- js/excersises.test.js
+++ js/excersises.test.js
@@ -1,5 +1,117 @@
-store(1)
-retrieve() // returnerar 1
-store(2)
-store(100)
-retrieve() // returnerar 100
+const script = require('./excersises.js');
+const {
+  store,
+  retrieve,
+  capitalize,
+  multiply,
+  isWaterBoiling,
+  intToRoman
+} = script;
+
+
+//*** Excersise 1 & 2 ***
+it('should store a number then return it(1)', () => {
+  // jest har funktionerna it, expect och toBe
+
+  //förbereda testet, Arrange
+  const x = 1;
+
+  //kalla funktionerna som ska testas, Act
+  store(x)
+  let expected = retrieve(); // returnerar 1
+
+  //kör själva testet, Assert
+  expect(x).toBe(expected);
+})
+
+it('should return the latest number passed to store(100)', () => {
+  //förbereda testet, Arrange
+  const x = 2,
+    y = 100;
+
+  //kalla funktionerna som ska testas, Act
+  store(x);
+  store(y);
+  let expected = retrieve(); // returnerar 100
+
+  //kör själva testet, Assert
+  expect(y).toBe(expected);
+})
+
+//*** Excersise 3 ***/
+it('pass a string to a function which returns the string with a capital first letter', () => {
+  //Arrange
+  let string = 'hej kära barn';
+
+  //Act
+  let expected = 'Hej kära barn';
+  let actual = capitalize(string)
+
+  //Assert
+  expect(actual).toBe(expected);
+});
+
+//*** Exercise 4 ***/
+it('pass 2 numbers to a function which multiplies them and returns the result', () => {
+  //Arrange
+  const x = 10,
+    y = 20;
+
+  //Act
+  let expected = 10 * 20;
+  let actual = multiply(x, y);
+
+  //Assert
+  expect(actual).toBe(expected);
+})
+
+//*** Excersise 5 ***/
+it('checks if water is hot enough to boil, returns true if over 100c', () => {
+  //Arrange
+  const degreesCelsius = 106;
+  let expected;
+  const actual = true;
+
+  //Act
+  expected = isWaterBoiling(degreesCelsius)
+
+  //Assert
+  expect(actual).toBe(expected)
+})
+
+it('checks if water is hot enough to boil, returns false', () => {
+  //Arrange
+  const degreesCelsius = 90;
+  let expected;
+  const actual = false;
+
+  //Act
+  expected = isWaterBoiling(degreesCelsius)
+
+  //Assert
+  expect(actual).toBe(expected)
+})
+
+it('checks if input sent to function gets thrown', () => {
+  //Arrange
+  const degreesCelsius = '';
+
+  //Act
+  let maybeError = () => isWaterBoiling(degreesCelsius);
+
+  //Assert
+  expect(maybeError).toThrow();
+})
+
+//*** Excersise 6 ***/
+it("expect swingToRoman to return to roman numbers not numbers ", () => {
+  //Arrange
+  const number = "3";
+  let actual = "III";
+
+  //Act
+  expected = intToRoman(number);
+
+  //Assert
+  expect(actual).toBe(expected);
+});
