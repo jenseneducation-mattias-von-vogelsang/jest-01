d1f611a0e974fe3fe0accc125e2c94ec564b4e27
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-08-17 11:05:57 +0200

    first commit

---
+++ js/excersises.js
@@ -0,0 +1,14 @@
+// let number;
+
+// function store(input) {
+//   number = input;
+// }
+
+// function retrieve() {
+//   return number;
+// }
+
+// export default {
+//   store,
+//   retrieve
+// };
---
+++ js/excersises.test.js
@@ -0,0 +1,5 @@
+store(1)
+retrieve() // returnerar 1
+store(2)
+store(100)
+retrieve() // returnerar 100
